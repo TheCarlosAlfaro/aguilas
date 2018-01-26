2f0c1d466f23bb16d39154a1e3e86f9d6ca4b93e
devbysalas <rgkalin@gmail.com>
2018-01-25 18:33:20 -0800

    Fixed logo size to be responsive and move the jumbotron to a part

---
+++ parts/jumbotron.php
@@ -0,0 +1,16 @@
+<div id="primary" class="content-area">
+    <main id="main" class="site-main">
+
+        <section class="jumbotron text-center">
+            <div class="container">
+                <h1 class="jumbotron-heading">Aguilas Theme</h1>
+                <p class="lead text-muted">The official WordPress theme for Aguilas Centro Familiar Cristiano.</p>
+                <p>
+                    <a href="#" class="btn btn-primary my-2">Get it now!</a>
+                    <a href="#" class="btn btn-secondary my-2">Learn More.</a>
+                </p>
+            </div>
+        </section>
+
+    </main><!-- #main -->
+</div><!-- #primary -->
